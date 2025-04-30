0ddb09eb4da8b32587e50d4e7dbdca8c7f329477
dyda98 <khadijabinali98@gmail.com>
2025-04-30 11:24:04 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+
+// Function to open the sidebar
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+// Function to close the sidebar
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
